f3724efc293130441d4ff5367d480bf6f346601f
godfrey <godfrey@scalpel.ai>
2021-06-14 06:13:38 +0100

    Added poetry files

--- setup.py
+++ setup.py
@@ -1,22 +1,28 @@
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+import setuptools
 
-from setuptools import setup, find_packages
+if __name__ == "__main__":
+    setuptools.setup()
 
-
-with open('README.rst') as f:
-    readme = f.read()
-
-with open('LICENSE') as f:
-    license = f.read()
-
-setup(
-    name='sample',
-    version='0.1.0',
-    description='Sample package for Python-Guide.org',
-    long_description=readme,
-    author='Kenneth Reitz',
-    author_email='me@kennethreitz.com',
-    url='https://github.com/kennethreitz/samplemod',
-    license=license,
-    packages=find_packages(exclude=('tests', 'docs'))
-)
+# # -*- coding: utf-8 -*-
+#
+# from setuptools import setup, find_packages
+#
+#
+# with open('README.rst') as f:
+#     readme = f.read()
+#
+# with open('LICENSE') as f:
+#     license = f.read()
+#
+# setup(
+#     name='sample',
+#     version='0.1.0',
+#     description='Sample package for Python-Guide.org',
+#     long_description=readme,
+#     author='Kenneth Reitz',
+#     author_email='me@kennethreitz.com',
+#     url='https://github.com/kennethreitz/samplemod',
+#     license=license,
+#     packages=find_packages(exclude=('tests', 'docs'))
+# )
